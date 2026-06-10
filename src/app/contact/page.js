295ad940938777040ea6b3c2089d29daf95525d9
Sudeep Das <sudeepdas2525@zohomail.in>
2026-06-10 17:05:11 +0530

    newpanal1

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { MapPin, Phone, Mail, Clock, Send, MessageSquareHeart } from "lucide-react"
 
